475f233956524e8cec6e0c17f541b6c13396a925
RKCP7668 <143069426+RKCP7668@users.noreply.github.com>
2023-09-26 10:56:59 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,16 @@
+
+//     -----------Name:- RISHABH PANDEY --------Univ. Roll No:- 2215001445 ----------
+
+import java.util.Scanner;
+class Test
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter your first number:");
+		int a=sc.nextInt();
+		System.out.print("Enter your second number:");
+		int b=sc.nextInt();
+		System.out.println("Sum of a and b is"+a+b);	
+	}
+}
