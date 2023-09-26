2d2b776869abbbba59c6a317a02918280bf12b56
RKCP7668 <143069426+RKCP7668@users.noreply.github.com>
2023-09-26 11:03:44 +0530

    Update div6and7.java

--- div6and7.java
+++ div6and7.java
@@ -1,6 +1,3 @@
-
-//     -----------Name:- Rajeev Chaudhary --------Univ. Roll No:- 2215001417 ----------
-
 import java.util.Scanner;
 public class div6and7 {
     public static void main(String[] args)
