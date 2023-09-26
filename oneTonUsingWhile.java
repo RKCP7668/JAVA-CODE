6cf9992e40fd6e164598b7c2474b8c28dd8d3eba
RKCP7668 <143069426+RKCP7668@users.noreply.github.com>
2023-09-26 10:53:42 +0530

    Create oneTonUsingWhile.java

---
+++ oneTonUsingWhile.java
@@ -0,0 +1,18 @@
+
+//     -----------Name:- Rajeev Chaudhary --------Univ. Roll No:- 2215001417 ----------
+
+import java.util.Scanner;;
+public class oneTonUsingWhile {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int num = sc.nextInt();
+        int i=1;
+        while(i<=num)
+        {
+            System.out.print(i+" ");
+            i++;
+        }
+    }
+}
