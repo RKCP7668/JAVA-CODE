d42d76c65cfe5f54aa298b8d40534298f5354f84
RKCP7668 <143069426+RKCP7668@users.noreply.github.com>
2023-09-26 10:43:02 +0530

    Create areaOfRect.java

---
+++ areaOfRect.java
@@ -0,0 +1,16 @@
+
+//     -----------Name:- Rajeev Chaudhary --------Univ. Roll No:- 2215001417 ----------
+
+import java.util.Scanner;
+
+public class areaOfRect {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+		System.out.print("Enter length of rectangle:");
+        float l=sc.nextFloat();
+        System.out.print("Enter width of rectangle:");
+        float w=sc.nextFloat();
+        System.out.print(l*w);
+    }
+}
