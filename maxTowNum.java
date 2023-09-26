56ff5c72a1c41281dc3fc44babcff50cc58979d1
RKCP7668 <143069426+RKCP7668@users.noreply.github.com>
2023-09-26 10:50:08 +0530

    Create maxTowNum.java

---
+++ maxTowNum.java
@@ -0,0 +1,16 @@
+// 1.	Write a  Java program to find maximum between two numbers.
+
+import java.util.Scanner;
+public class maxTowNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number:");
+        int fnum = sc.nextInt();
+        System.out.print("Enter second number:");
+        int snum = sc.nextInt();
+        if (fnum > snum)
+            System.out.println(fnum +" is maximum.");
+        else
+            System.out.println(snum +" is maximum.");
+    }
+}
