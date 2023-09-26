cd3a861e2c4c525c62bb6e8348ff57ebc73186b5
RKCP7668 <143069426+RKCP7668@users.noreply.github.com>
2023-09-26 10:44:09 +0530

    Create evenOdd.java

---
+++ evenOdd.java
@@ -0,0 +1,19 @@
+
+//     -----------Name:- Rajeev Chaudhary --------Univ. Roll No:- 2215001417 ----------
+
+import java.util.Scanner;
+
+public class evenOdd {
+    public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter to check  your number is even or odd:");
+		int num=sc.nextInt();
+		if (num==0)	
+            System.out.println(num+" is zero");
+        else if(num%2==0)
+            System.out.println(num+" is even"); 
+        else
+            System.out.println(num+" is odd");      
+	}
+}
