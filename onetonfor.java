68f6415771cb217567ddd450260498f69f7505e8
RKCP7668 <143069426+RKCP7668@users.noreply.github.com>
2023-09-26 10:54:58 +0530

    Create onetonfor.java

---
+++ onetonfor.java
@@ -0,0 +1,17 @@
+
+//     -----------Name:- Rajeev Chaudhary --------Univ. Roll No:- 2215001417 ----------
+
+import java.util.Scanner;
+
+public class onetonfor {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int num = sc.nextInt();
+        for(int i=1;i<=num; i++)
+        {
+            System.out.print(i+" ");
+        }
+    }
+}
